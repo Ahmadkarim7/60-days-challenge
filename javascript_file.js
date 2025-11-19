74cab7b94dcd8f5093682426075e92d8ed61af41
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2025-11-18 21:43:47 -0500

    Add files via upload

---
+++ javascript_file.js
@@ -0,0 +1,49 @@
+alert("Hello "+ "Wolrd");
+//let var data type
+//length to count the length of the variable
+var nome = "Donley";
+nome.length;
+console.log(nome.length);
+alert("Hello");
+
+//Compose input (it will open a window in the top like alert to enter anyting)
+prompt("Enter your name");
+
+//slice and dise your strings
+var secondname = "Robert";
+secondname.slice(0,1); //0 is the index and 1(not included 1) is the character. so only print R
+//slice (starting position, ending position)
+secondname.slice(0,140);//it does not allow the input to be exceeded 140 characters
+
+//toUpperCase()-method that changes all characters in upper case
+secondname.toUpperCase();
+
+//pracctice
+var thirdname = prompt("Enter your name");
+var firstchar = thirdname.slice(0,1);
+var upperCastFirstChar = firstchar.toUpperCase();
+var restOfName = thirdname.slice(1, thirdname.length); //whole idea is to make the first character upper case lever from entering name from the input
+
+
+//Numbers  + - / * % (modules=reminder)
+//increament decrement x = x + 1; x++; or x--; x = x + y; - x +=y;
+
+
+//functions function name (){}
+
+//function and parameters
+//Three versions of functions --without parameter --  with parameter -- 
+
+// 12.333 round down function floor() Math.floor
+
+//challenge check how many days, weeks, and months are remaining if you live 90 years
+function lifeRemainintg(age){
+    var yearsremainng = 90 - age;
+    var months = yearsremainng * 12;
+    var week = yearsremainng * 52;
+    var days = yearsremainng * 365;
+    console.log("Remaining months are" + months +  "days are"  + days +  "and weeks are" + week );
+}
+lifeRemainintg(20);
+
+
