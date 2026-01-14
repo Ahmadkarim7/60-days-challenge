004aeaf910519501903833f608e64480a6c9d0d9
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2026-01-14 02:53:18 -0500

    Add files via upload

---
+++ Express Middleware/index.js
@@ -0,0 +1,13 @@
+//Man in the Middle
+//It sits between server and Routes
+
+//Preprocess the request
+
+//Body parser-- body passing middleware. It is used to handle form data---like when you login then you want to check the email and password --- thats where Body parser comes to Play!
+
+//--> Form in HTML--- actio is the route and method is the way like post---> where you want to check data!
+
+//In public file we put static files such as Images which does not change
+
+//How to use Middle war (Body parser)
+//Install Body parser
