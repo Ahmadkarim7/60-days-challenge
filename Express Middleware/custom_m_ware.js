a4295e1fd10801bdf66194fb64aac813d81ed129
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2026-01-14 03:59:56 -0500

    Add files via upload

---
+++ Express Middleware/custom_m_ware.js
@@ -0,0 +1,12 @@
+//FFour types of Middle ware 
+//Logging ---> one other package called Morgan
+//Pre-Processor
+//Authentication
+//Error Handling
+
+//npm morgan--> used to log the request which is coming from the server
+//in custome middle ware we use req, res, next methods because sometimes we have multiple middlewares where we want to keep moving
+
+//we use postmon instead of browser
+
+//app.use() runs something before routes for every request.
--- Express Middleware/index2.js
+++ Express Middleware/index2.js
@@ -1,7 +1,15 @@
 import express from "express";
+import morgan from "morgan";  //can also use require methodd instead of import but both works
 
 const app = express();
 const port = 3000;
+app.use(morgan("tiny"));
+
+app.use((req, res, next)=> {
+  console.log("Request method: ", req.method);
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
