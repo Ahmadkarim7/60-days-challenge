9e557c36a5f6a2cb343d2e4ffc17f29c15df2be6
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2026-01-27 01:42:57 -0500

    Add files via upload

---
+++ API + JSON/notes.js
@@ -0,0 +1,57 @@
+//Application Program Interface-- how different teachnologies work together or interact different softwares together. 
+
+//types of API's
+//Architecual Style (house door analogyß)
+// Web dev {REST: API} 
+//Rest APi: HTTP protocols 
+
+//Formatting API requests
+//
+
+
+//Endpoint = a route where the client talks to the server
+
+//Path Parammeter
+//base url /endpoint/paht parameter
+//to find somoe specific itemxs 
+
+
+//Query parameter
+//base url / endpoint ? query=value
+//you can have multiple query paramenters with & plus second query
+
+
+//Server to Server talking ---> Public API-- we do by using Get method
+
+//API documentation
+//http://bored-api.appbrewery.com
+
+//API Endpoint -- baseURLl/Endpoint
+//base url first part then Endpoint
+
+//Path parameters identify the resource being requested, while query parameters modify or filter how that resource is returned. This separation keeps APIs clean, scalable, and easy to understand.
+
+//Path parameters identify WHAT resource is being requested, and query parameters define HOW that resource should be returned. They allow one API endpoint to serve many real-world use cases efficiently.
+
+//Intro to JSON
+//JavaScript Object notation
+ //
+ /*
+ in js 
+ const jack = {
+ name: "Khan",
+ age: 40,
+}
+In JSON
+{
+"name": "Khan:"
+"age": 45:
+//*
+ */
+ 
+
+
+//json viewver so you can see the json files in order
+
+
+//Pattern monster can be used for css backgrounds
