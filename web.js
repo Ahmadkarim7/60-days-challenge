34944a24ae6370a8ec9bfc15052de09c731eb1b2
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2026-02-25 15:04:00 -0500

    Update web.js

--- web.js
+++ web.js
@@ -12,6 +12,7 @@
 //
 ////
 ////
+//
 //nesting and Identition --ul list inside ul list
 // which means  list inside the list
 //<a> we use it for link like <a href="link"> content </a> dragable=true (attribut)
@@ -270,6 +271,7 @@ Why: Absolute always looks to the nearest positioned ancestor — fixed counts a
 //No change
 //
  
+
 
 
 
